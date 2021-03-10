04ba8e2ba6faebf08aa503d5f679fb78789fc8c7
IzzatSolarAi <izzkhair@gmail.com>
2021-03-10 11:36:15 +0800

    Added the address endpoint and fields

--- functions/index.js
+++ functions/index.js
@@ -2,74 +2,89 @@
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
-const firestore=admin.firestore();
+const firestore = admin.firestore();
 
-exports.addNewUserPhone=functions.https.onCall((data, context)=>{
-  const UID=data.uid;
-  const phoneNo= data.phoneNo;
-  const user=firestore.collection("elderlyUsers").doc(UID);
-  const returnData = user.get().then((doc) => {
-    if (doc.exists) {
-      console.log({
-        msg: "You are an existing user!",
-        isPreviouslyUser: true,
-        status: 200,
-        UID: UID,
-        PhoneNo: phoneNo,
+exports.addNewUserPhone = functions.https.onCall((data, context) => {
+  const UID = data.uid;
+  const phoneNo = data.phoneNo;
+  const user = firestore.collection("elderlyUsers").doc(UID);
+  const returnData = user
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          const data = doc.data();
+          console.log({
+            profileData: data,
+            status: "200",
+            msg: "You are an existing user!",
+            isPreviouslyUser: true,
+            isSignUpCompleted: Boolean(doc.data().address),
+          });
+
+          return {
+            profileData: data,
+            status: "200",
+            msg: "You are an existing user!",
+            isPreviouslyUser: true,
+            isSignUpCompleted: Boolean(doc.data().address),
+          };
+        } else {
+          const newUserReturnData = firestore
+              .collection("elderlyUsers")
+              .doc(UID)
+              .set({
+                uid: UID,
+                phoneNo: phoneNo,
+              })
+              .then(() => {
+                console.log( {
+                  msg: "You are new user, your UID and PhoneNo added to our database",
+                  status: 200,
+                  isPreviouslyUser: false,
+                  profileData: {
+                    UID: UID,
+                    PhoneNo: phoneNo,
+                  },
+                });
+
+                return {
+                  msg: "You are new user, your UID and PhoneNo added to our database",
+                  status: 200,
+                  isPreviouslyUser: false,
+                  profileData: {
+                    UID: UID,
+                    PhoneNo: phoneNo,
+                  },
+                };
+              });
+          return newUserReturnData;
+        }
+      })
+      .catch((error) => {
+        console.log("Error getting document:", error);
       });
-
-      return {
-        msg: "You are an existing user!",
-        isPreviouslyUser: "true",
-        status: 200,
-        UID: UID,
-        PhoneNo: phoneNo,
-      };
-    } else {
-      const newUserReturnData= firestore.collection("elderlyUsers").doc(UID).set({
-        uid: UID,
-        phoneNo: phoneNo,
-      }).then(()=>{
-        console.log({
-          msg: "You are new user, your UID and PhoneNo added to our database",
-          isPreviouslyUser: false,
-          status: 200,
-          UID: UID,
-          PhoneNo: phoneNo,
-        });
-
-        return {
-          msg: "You are new user, your UID and PhoneNo added to our database",
-          isPreviouslyUser: "false",
-          status: 200,
-          UID: UID,
-          PhoneNo: phoneNo,
-        };
-      });
-      return newUserReturnData;
-    }
-  }).catch((error) => {
-    console.log("Error getting document:", error);
-  });
 
   return returnData;
 });
 
-exports.addNewUserFinalStep=functions.https.onCall(async (data, context) =>{
-  const uid=data.uid;
-  const name=data.name;
+exports.addNewUserFinalStep = functions.https.onCall(async (data, context) => {
+  const uid = data.uid;
+  const name = data.name;
   const profilePicUrl = data.profilePicUrl;
-  const user=firestore.collection("elderlyUsers").doc(uid);
+  const user = firestore.collection("elderlyUsers").doc(uid);
 
-  const status = await user.get()
-      .then( (doc) => {
+  const status = await user
+      .get()
+      .then((doc) => {
         return doc;
-      }).then(async (doc)=>{
+      })
+      .then(async (doc) => {
         if (doc.exists) {
-          const returnStatus= await user.update({
-            name: name,
-            profilePicUrl: profilePicUrl,
-          })
+          const returnStatus = await user
+              .update({
+                name: name,
+                profilePicUrl: profilePicUrl,
+              })
               .then(() => {
                 console.log("Document successfully updated!");
                 // result="Document successfully updated!";
@@ -83,14 +98,17 @@ exports.addNewUserFinalStep=functions.https.onCall(async (data, context) =>{
               });
           return returnStatus;
         }
-      }).catch((error) => {
+      })
+      .catch((error) => {
         console.log("Error getting document:", error);
       });
 
-  const updateData= await user.get()
-      .then( (doc) => {
+  const updateData = await user
+      .get()
+      .then((doc) => {
         return doc.data();
-      }).catch((error) => {
+      })
+      .catch((error) => {
         console.log("Error getting document:", error);
       });
 
@@ -98,3 +116,50 @@ exports.addNewUserFinalStep=functions.https.onCall(async (data, context) =>{
 });
 
 
+exports.addNewUserAddress = functions.https.onCall(async (data, context) => {
+  const uid = data.uid;
+  const address = data.address;
+  const unitNo = data.unitNo;
+  const user = firestore.collection("elderlyUsers").doc(uid);
+
+  const status = await user
+      .get()
+      .then((doc) => {
+        return doc;
+      })
+      .then(async (doc) => {
+        if (doc.exists) {
+          const returnStatus = await user
+              .update({
+                address: address,
+                unitNo: unitNo,
+              })
+              .then(() => {
+                console.log("Document successfully updated!");
+                // result="Document successfully updated!";
+                return "Document successfully updated!";
+              })
+              .catch((error) => {
+                // The document probably doesn't exist.
+                console.error("Error updating document: ", error);
+                // result="Error updating document: ";
+                return "Error updating document: ";
+              });
+          return returnStatus;
+        }
+      })
+      .catch((error) => {
+        console.log("Error getting document:", error);
+      });
+
+  const updateData = await user
+      .get()
+      .then((doc) => {
+        return doc.data();
+      })
+      .catch((error) => {
+        console.log("Error getting document:", error);
+      });
+
+  return {status: status, profileData: updateData};
+});
