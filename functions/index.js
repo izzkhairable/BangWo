71e0db0e6b13eb899009b76215e35796facbc995
IzzatSolarAi <izzkhair@gmail.com>
2021-03-17 23:26:25 +0800

    Organised the my testing  HTML files
    All of my testing HTML files will be places using /testing folder for both elderly and volunteer. So that Josh and Sebastin file will be the main one.
    
    Also Sheffield, I adjected one line for stickers in the index.js.

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,7 @@
 const admin = require("firebase-admin");
-const { user } = require("firebase-functions/lib/providers/auth");
-const { ExportBundleInfo } = require("firebase-functions/lib/providers/analytics");
 admin.initializeApp();
 exports.signUpElderly = require("./signUpElderly");
 exports.signUpVolunteer = require("./signUpVolunteer");
 exports.tasks = require("./tasks");
 exports.checkAccountType=require("./checkAccountType");
-exports.stickers = require("./tasks")
+exports.stickers = require("./stickers");
