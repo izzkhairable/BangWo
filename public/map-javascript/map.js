a15b5b80858f83b4a3cd477ed94c02c4a8d988fc
Josh Tseng <joshua.tseng.sh@gmail.com>
2021-03-22 19:18:00 +0800

    Added map functionality to task-arrived page for volunteer, edited Javascript map functionality
    - Included map to identify user's current location when hit "arrived" button in task menu
    - Edited Javascript handling map functionality to be able to allow for both finding directions and identifying user location

--- public/map-javascript/map.js
+++ public/map-javascript/map.js
@@ -8,6 +8,29 @@ function retrieveApiKey() {
 	return "AIzaSyBUcwKDHwnPlhLlJBZCNulc-abORf42qdA";
 }
 
+//getLocation() accepts a string instruction to determine what type of Google Map is displayed
+//If param is "directions", it will display directions to go from origin to destination
+//If param is "user-location", it will display user's current location
+function getLocation(instruction) {
+	if (navigator.geolocation) {
+		if (instruction=="directions") {
+			navigator.geolocation.getCurrentPosition(displayMapDirections);
+		} else if (instruction=="user-location") {
+			navigator.geolocation.getCurrentPosition(displayCurrentLocation);
+		}
+	} else {
+		document.getElementById("map-display").innerHTML = "<h3>Geolocation is not supported by this browser.</h3>";
+	}
+}
+
+function displayCurrentLocation(user_location) {
+	var url = "https://www.google.com/maps/embed/v1/place";
+	var api_key = "?key=" + retrieveApiKey();
+	var query = "&q=" + user_location.coords.latitude + "," + user_location.coords.longitude;
+
+document.getElementById("map-display").setAttribute("src", url + api_key + query);	
+}
+
 function displayMapDirections(user_location) {
 	var url = "https://www.google.com/maps/embed/v1/directions";
 	var api_key = "?key=" + retrieveApiKey();
@@ -16,13 +39,4 @@ function displayMapDirections(user_location) {
 
 document.getElementById("map-display").setAttribute("src", url + api_key + origin +destination);	
 }
-
-function getLocation() {
-	if (navigator.geolocation) {
-		navigator.geolocation.getCurrentPosition(displayMapDirections);
-	} else {
-		document.getElementById("map-display").innerHTML = "<h3>Geolocation is not supported by this browser.</h3>";
-	}
-}
-
 
