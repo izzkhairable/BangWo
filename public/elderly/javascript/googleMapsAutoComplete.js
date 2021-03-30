9315242a63a089575dfd3fbe55135ae4a5f0c0db
IzzatSolarAi <izzkhair@gmail.com>
2021-03-31 04:17:33 +0800

    Fixed issues with map also included businesses in the location search

--- public/elderly/javascript/googleMapsAutoComplete.js
+++ public/elderly/javascript/googleMapsAutoComplete.js
@@ -5,7 +5,7 @@
       autocomplete = new google.maps.places.Autocomplete(address1Field, {
         componentRestrictions: { country: ["sg"] },
         fields: ["address_components", "geometry"],
-        types: ["address"],
+        types: ["geocode","establishment"],
       });
       address1Field.focus();
     }
