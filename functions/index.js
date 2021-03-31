db76a0f18afb0e63a376ea930a68ed450ad47992
Sheffield Lok <yclok.2019@sis.smu.edu.sg>
2021-03-31 13:36:20 +0800

    added index.js line 10

--- functions/index.js
+++ functions/index.js
@@ -7,9 +7,4 @@ exports.signUpVolunteer = require("./signUpVolunteer");
 exports.tasks = require("./tasks");
 exports.checkAccountType=require("./checkAccountType");
 exports.stickers = require("./stickers");
-<<<<<<< HEAD
 exports.chats = require("./chats");
-=======
-
-
->>>>>>> 03db89f95a82dff2ef6e573f03c3381df3e09223
