e66d729354437b6aab1596172431cb9e6f88f86f
IzzatSolarAi <izzkhair@gmail.com>
2021-04-16 23:41:11 +0800

    Added JS Docs to stickers functions

--- functions/stickers.js
+++ functions/stickers.js
@@ -3,7 +3,12 @@ const admin = require("firebase-admin");
 const firestore = admin.firestore();
 /* eslint-disable max-len */
 
-// Elderly give sticker
+/**
+ * Add sticker to an array of stickers in the volunteer user profile in Firebase
+ * @param {string} volunteerId - The unique id of the volunteer user, each uid is unique regardless of user type.
+ * @param {string} stickerName - The name of the sticker to be added to volunteer user profile
+ * @returns {object} An object containing response status an updated array of the volunteer stickers
+ */
 exports.addSticker = functions.https.onCall(async (data) => {
   const volunteerId = data.volunteerId;
   const stickerName=data.stickerName;
@@ -12,7 +17,6 @@ exports.addSticker = functions.https.onCall(async (data) => {
       .get()
       .then(async (doc) => {
         if (doc.exists) {
-          // pull quantity of each sticker in to a variable
           const award = await volunteer.get().then((doc)=>{
             console.log(doc.data());
             let stickers=[];
@@ -24,35 +28,38 @@ exports.addSticker = functions.https.onCall(async (data) => {
               stickers: stickers,
             })
                 .then(() => {
-                  console.log("You have given a verygood sticker!");
+                  console.log(`Volunteer successfully awarded the ${stickerName} sticker!`);
                   return {
                     status: 200,
                     msg: stickers,
                   };
                 })
                 .catch((error) => {
-                  console.error("Error giving sticker ", error);
-                  return "Error giving sticker";
+                  console.error(`Error giving the ${stickerName} sticker`, error);
+                  return `Error giving the ${stickerName} sticker`;
                 });
-          }).catch((err)=>{
-            console.error("Error giving sticker ");
-            return "Error giving sticker";
+          }).catch((error)=>{
+            console.error(`Error giving the ${stickerName} sticker`, error);
+            return `Error giving the ${stickerName} sticker`;
           });
           return award;
         }
       })
       .catch((error) => {
-        console.log(error);
-        console.log("Could not find sticker, perhaps sticker does not exist");
+        console.error(`Error giving the ${stickerName} sticker`, error);
+        return `Error giving the ${stickerName} sticker`;
       });
   return addSticker;
 });
 
-// Get all stickers by volunteer
+/**
+ * Get all sticker from Firebase by volunteerId
+ * @param {string} volunteerId - The unique id of the volunteer user, each uid is unique regardless of user type.
+ * @returns {object} An object containing volunteer stickers
+ */
 exports.getStickersByVolunteer = functions.https.onCall(async (data) => {
   const volunteerId = data.volunteerId;
   const volunteer = firestore.collection("volunteerUsers").doc(volunteerId);
-
   const returnStickersByVolunteer = await volunteer
       .get()
       .then((doc) => {
@@ -62,11 +69,13 @@ exports.getStickersByVolunteer = functions.https.onCall(async (data) => {
             stickers: result.stickers,
           };
         } else {
-          console.log("This volunteer has no stickers yet!");
+          console.log("Volunteer has no stickers yet!");
+          return "Volunteer has no stickers yet!";
         }
       })
       .catch((error) => {
-        console.log("Error retrieving this volunteer", error);
+        console.log("Error retrieving volunteer stickers", error);
+        return "Error retrieving volunteer stickers";
       });
   return returnStickersByVolunteer;
 });
