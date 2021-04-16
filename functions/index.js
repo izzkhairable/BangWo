73c305adde7788ffef04f34de0dbbeb8c8283b52
IzzatSolarAi <izzkhair@gmail.com>
2021-04-16 22:55:10 +0800

    Added generic JS Docs to index

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,16 @@
+/* eslint-disable max-len */
 const admin = require("firebase-admin");
 admin.initializeApp();
+
+/**
+ * The main function file that will invoke other functions in other files
+ * To any function in the various files just simply fileName-nameOfFunction e.g. advice-getAdviceByElderly
+ */
 exports.signUpElderly = require("./signUpElderly");
 exports.signUpVolunteer = require("./signUpVolunteer");
-exports.tasks = require("./tasks");
-exports.checkAccountType=require("./checkAccountType");
-exports.stickers = require("./stickers");
-exports.chats = require("./chats");
-exports.advice = require("./advice");
 exports.volunteerProfile = require("./volunteerProfile");
+exports.checkAccountType=require("./checkAccountType");
+exports.tasks = require("./tasks");
+exports.stickers = require("./stickers");
+exports.advice = require("./advice");
+
