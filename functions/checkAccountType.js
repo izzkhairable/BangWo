a20ffe99bcdd6cca6cf50a8b3f5b1ec1e4cc9f61
IzzatSolarAi <izzkhair@gmail.com>
2021-04-16 22:50:59 +0800

    Added JS Docs to checkAccountType function

--- functions/checkAccountType.js
+++ functions/checkAccountType.js
@@ -1,10 +1,16 @@
+/* eslint-disable max-len */
 const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 const firestore = admin.firestore();
 
+/**
+ * The function purpose is to check the type of volunteer in Firestore collection
+ * In the frontend, if the user is a volunteer and attempts to login as an elderly would be redirected to the volunteer page(vice versa)
+ * @param {string} uid - The unique id of the user, each uid is unique regardless of user type
+ * @returns {string} The type of volunteer (elderly, volunteer or none)
+ */
 exports.checkAccountType = functions.https.onCall(async (data) => {
   const uid=data.uid;
-  console.log("This is UID", uid);
   const volunteer= firestore.collection("volunteerUsers").doc(uid);
   const elderly= firestore.collection("elderlyUsers").doc(uid);
 
